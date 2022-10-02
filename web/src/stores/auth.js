2b6b04b8f2725a7a1491d0d0a418aa6f0c153534
James C. Tucker <hellojamestucker@gmail.com>
2022-10-02 17:54:55 -0500

    setup auth store

---
+++ web/src/stores/auth.js
@@ -0,0 +1,32 @@
+import { ref, computed } from "vue";
+import { defineStore } from "pinia";
+import { useAuth } from "@/composables/auth/useAuth";
+export const useAuthStore = defineStore("auth", () => {
+  const { register, login, logout, getUser } = useAuth();
+  const initialState = { user: getUser() };
+  const user = ref({ ...initialState });
+
+  const isAuthenticated = computed(() => {
+    return !!user.value.user;
+  });
+
+  const createUser = async ({ email, password } = {}) => {
+    await register({ email, password }).then((response) => {
+      user.value.user = response.data;
+    });
+  };
+
+  const signIn = async ({ email, password } = {}) => {
+    const response = await login({ email, password });
+    user.value.user = response.data;
+
+    return { response };
+  };
+  const signOut = async () => {
+    await logout().then(() => {
+      user.value.user = null;
+    });
+  };
+
+  return { user, isAuthenticated, createUser, signIn, signOut };
+});
