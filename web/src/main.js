e820ba12686697a1b13b097361bfe09c73eec0e2
James C. Tucker <hellojamestucker@gmail.com>
2022-10-02 17:41:03 -0500

    setup vue query

--- web/src/main.js
+++ web/src/main.js
@@ -1,14 +1,17 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import { VueQueryPlugin } from "vue-query";
+import { queryClient } from "@/queries/query-client";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 
-import './assets/main.css'
+import "./assets/main.css";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia());
+app.use(VueQueryPlugin, { queryClient });
+app.use(router);
 
-app.mount('#app')
+app.mount("#app");
---
+++ web/src/queries/queryClient.js
@@ -0,0 +1,10 @@
+import { QueryClient } from "vue-query";
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+    },
+  },
+});
